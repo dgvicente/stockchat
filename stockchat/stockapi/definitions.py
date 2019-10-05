7c4eb31e92cb2d3e601924be43abb85c1a02d5f8
Diana <dianagv@gmail.com>
2019-10-05 17:11:34 -0500

    refactors

---
+++ stockchat/stockapi/definitions.py
@@ -0,0 +1,3 @@
+STOCK_ELEMENT_ROW = 1
+STOCK_ELEMENT_INDEX = 3
+API_URL = 'https://stooq.com/q/l/?s=%s&f=sd2t2ohlcv&h&e=csv'
--- stockchat/stockapi/views.py
+++ stockchat/stockapi/views.py
@@ -1,7 +1,6 @@
 import json
 
 from django.http import HttpResponse
-from django.template.loader import render_to_string
 
 from .api_client import StockApiClient
 
